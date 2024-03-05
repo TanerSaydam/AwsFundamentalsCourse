99acac4b6e3df6a874ddcd968df8028d0b439da0
TanerSaydam <tanersaydam@gmail.com>
2024-03-05 09:57:42 +0300

    Add project files.

---
+++ 1.SQS/SQSPublisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+string accessKey = "";
+string secretKey = "";
+var region = Amazon.RegionEndpoint.USEast1;
+
+var sqsClient = new AmazonSQSClient(region);
+
+var customer = new
+{
+    FirstName = "Taner",
+    LastName = "Saydam",
+    Age = 34
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = "Customer"
+            }
+        }
+    },
+    
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.ReadLine();
