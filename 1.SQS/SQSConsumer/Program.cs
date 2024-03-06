fdbc46f7bb2acbceef2249c26db63df6b4cbb188
TanerSaydam <tanersaydam@gmail.com>
2024-03-07 00:19:51 +0300

    Message Consumer Uygulaması Oluşturalım

---
+++ 1.SQS/SQSConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>() { "All"},
+    MessageAttributeNames = new List<string>() { "All"}
+};
+
+var cts = new CancellationTokenSource();
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+
+    foreach (var message in response.Messages)
+    {
+        //Mail Gönder
+        //Sms Gönder
+
+        try
+        {
+            Console.WriteLine($"Message Id: {message.MessageId}");
+            Console.WriteLine($"Message Body: {message.Body}");
+
+            //await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+        
+    }
+
+    await Task.Delay(100, cts.Token);
+}
