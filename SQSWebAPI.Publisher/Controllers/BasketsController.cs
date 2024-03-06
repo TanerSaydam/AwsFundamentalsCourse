171df99e0877859f0f9ae5f675d67068cc1c4d5b
TanerSaydam <tanersaydam@gmail.com>
2024-03-07 00:37:40 +0300

    API ile Çalışalım

---
+++ SQSWebAPI.Publisher/Controllers/BasketsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQSWebAPI.Publisher.Models;
+
+namespace SQSWebAPI.Publisher.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BasketsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> CreateOrder()
+    {
+        List<Basket> baskets = Basket.GetAll();
+        List<Order> orders = new List<Order>();
+
+        foreach (var basket in baskets)
+        {
+            Order order = new()
+            {
+                Price = basket.Price,
+                ProductName = basket.ProductName,
+                Quantity = basket.Quantity,
+            };
+
+            orders.Add(order);
+        }
+
+        //DB İşlemleri
+
+        //Mail Gönderme
+
+        return Ok(new { Message = "Sipariş başarıyla oluşturuldu" });
+    }
+}
