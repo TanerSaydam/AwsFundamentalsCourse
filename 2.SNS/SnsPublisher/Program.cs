d4710668b26c8e5f2e1e4f09c178e622a6ca18b9
TanerSaydam <tanersaydam@gmail.com>
2024-03-20 06:34:02 +0300

    Message Publisher Oluşturalım

---
+++ 2.SNS/SnsPublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System.Text.Json;
+
+var message = new
+{
+    Name= "Taner Saydam",
+    Age= 35
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient(Amazon.RegionEndpoint.USEast1);
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(message),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = "User"
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
