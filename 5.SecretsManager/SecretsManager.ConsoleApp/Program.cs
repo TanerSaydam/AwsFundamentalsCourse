22c4d5c3b138e68c49cfe3dc0bea9c50b07f305f
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 15:52:59 +0300

    C# ile Secret Okuyalım

---
+++ 5.SecretsManager/SecretsManager.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var client = new AmazonSecretsManagerClient();
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "apikey"
+};
+var response = await client.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "apikey"
+};
+
+var describeResponse = await client.DescribeSecretAsync(describeSecretRequest);
+
+Console.ReadLine();
