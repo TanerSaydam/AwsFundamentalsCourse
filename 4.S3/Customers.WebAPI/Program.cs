8a96eed9f269d11c8a6304d06f5d68e0a916a462
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 14:51:31 +0300

    API Projemize S3 Desteği Ekleyelim

---
+++ 4.S3/Customers.WebAPI/Program.cs
@@ -0,0 +1,27 @@
+using Amazon.S3;
+using Customers.WebAPI.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IAmazonS3, AmazonS3Client>();
+builder.Services.AddScoped<CustomerRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 4.S3/Customers.WebAPI/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.S3.Model;
+
+namespace Customers.WebAPI.Repositories;
+
+public class CustomerRepository
+{
+    public async Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile file)
+    {
+
+    }
+
+    public async Task<GetObjectResponse> GetImageAsync(Guid id)
+    {
+
+    }
+
+    public async Task<DeleteObjectResponse> DeleteImageAsync(Guid id)
+    {
+
+    }
+}
