2bbe4d0734fe4d6466911d54339eb2563efae093
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 14:45:35 +0300

    C# ile Datayı İndirelim ve İşleyelim

--- 4.S3/S3Playground/Program.cs
+++ 4.S3/S3Playground/Program.cs
@@ -1,16 +1,34 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using System.Text;
 
 var s3Client = new AmazonS3Client();
+#region Upload
+//using var inputStream = new FileStream("C:/Test/AwsFundamentalsCourse/4.S3/S3Playground/1-02.jpg", FileMode.Open, FileAccess.Read);
 
-using var inputStream = new FileStream("C:/Test/AwsFundamentalsCourse/4.S3/S3Playground/1-02.jpg", FileMode.Open, FileAccess.Read);
+//var putObjectRequest = new PutObjectRequest
+//{
+//    BucketName = "tsfilesystem",
+//    Key = "images/logo.jpg",
+//    ContentType = "image/jpeg", //mime type
+//    InputStream = inputStream
+//};
 
-var putObjectRequest = new PutObjectRequest
+//await s3Client.PutObjectAsync(putObjectRequest);
+#endregion
+
+#region Download
+var getObjectRequest = new GetObjectRequest
 {
     BucketName = "tsfilesystem",
-    Key = "images/logo.jpg",
-    ContentType = "image/jpeg", //mime type
-    InputStream = inputStream
+    Key = "images/logo.jpg"
 };
+var response = await s3Client.GetObjectAsync(getObjectRequest);
 
-await s3Client.PutObjectAsync(putObjectRequest);
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+Console.WriteLine(text);
+#endregion
