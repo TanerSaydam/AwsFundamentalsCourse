2dc55c4ac22e64235db348f8d7be9a78f0e83b16
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 14:40:29 +0300

    C# ile Data Yükleyelim

---
+++ 4.S3/S3Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+using var inputStream = new FileStream("C:/Test/AwsFundamentalsCourse/4.S3/S3Playground/1-02.jpg", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "tsfilesystem",
+    Key = "images/logo.jpg",
+    ContentType = "image/jpeg", //mime type
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
