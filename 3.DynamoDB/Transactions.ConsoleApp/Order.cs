c0412443295f488fba0793efc69ddd32dc72c120
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 07:09:17 +0300

    Transactions

---
+++ 3.DynamoDB/Transactions.ConsoleApp/Order.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+public sealed class Order
+{
+    [JsonPropertyName("pk")]
+    public string PK { get; set; } = string.Empty;
+    [JsonPropertyName("sk")]
+    public string SK { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+}
+
---
+++ 3.DynamoDB/Transactions.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using System.Text.Json;
+
+AmazonDynamoDBClient dynamoDb = new AmazonDynamoDBClient();
+
+ShoppingCart shoppingCart = new()
+{
+    PK = "1",
+    SK = "1",
+    ProductName = "Domates"
+};
+
+Order order = new()
+{
+    PK = "1",
+    SK = "1",
+    ProductName = "Domates"
+};
+
+var asJson1 = JsonSerializer.Serialize(shoppingCart);
+var asJson2 = JsonSerializer.Serialize(order);
+
+var attributeMap1 = Document.FromJson(asJson1).ToAttributeMap();
+var attributeMap2 = Document.FromJson(asJson2).ToAttributeMap();
+
+var transactRequest = new TransactWriteItemsRequest
+{
+    TransactItems = new List<TransactWriteItem>
+    {
+        new()
+        {
+            Put = new()
+            {
+                TableName = "shopping-carts",
+                Item = attributeMap1
+            }
+        },
+        new()
+        {
+            Put = new()
+            {
+                TableName = "orders",
+                Item = attributeMap2
+            }
+        }
+    }
+};
+
+var response = await dynamoDb.TransactWriteItemsAsync(transactRequest);
