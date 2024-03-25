a662e8028dc73d6cc7500d4a45cab698114812a6
TanerSaydam <tanersaydam@gmail.com>
2024-03-25 16:03:07 +0300

    Secrets Versiyonlama

--- 5.SecretsManager/SecretsManager.ConsoleApp/Program.cs
+++ 5.SecretsManager/SecretsManager.ConsoleApp/Program.cs
@@ -3,19 +3,32 @@ using Amazon.SecretsManager.Model;
 
 var client = new AmazonSecretsManagerClient();
 
+var listSecretVersionRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "apikey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await client.ListSecretVersionIdsAsync(listSecretVersionRequest);
+
 var request = new GetSecretValueRequest
 {
-    SecretId = "apikey"
+    SecretId = "apikey",
+    //VersionStage = "AWSCURRENT"
+    VersionId = "eb259489-391d-4761-823b-c06354f48529"
 };
 var response = await client.GetSecretValueAsync(request);
 
 Console.WriteLine(response.SecretString);
 
-var describeSecretRequest = new DescribeSecretRequest
-{
-    SecretId = "apikey"
-};
 
-var describeResponse = await client.DescribeSecretAsync(describeSecretRequest);
 
-Console.ReadLine();
+//var describeSecretRequest = new DescribeSecretRequest
+//{
+//    SecretId = "apikey"
+   
+//};
+
+//var describeResponse = await client.DescribeSecretAsync(describeSecretRequest);
+
+//Console.ReadLine();
