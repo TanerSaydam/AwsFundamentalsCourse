984555cdd0456c07b2ca1f9bf1ec6edaa63568e4
TanerSaydam <tanersaydam@gmail.com>
2024-03-29 14:24:04 +0300

    Lambda ile DynamoDb Trigger

---
+++ 6.Lambda/SimpleDynamoDbLambda/src/SimpleDynamoDbLambda/Function.cs
@@ -0,0 +1,24 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleDynamoDbLambda;
+public class Function
+{
+    public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+        foreach (var record in dynamoEvent.Records)
+        {
+            context.Logger.LogInformation($"Event ID: {record.EventID}");
+            context.Logger.LogInformation($"Event Name: {record.EventName}");
+
+            context.Logger.LogInformation($"New Image: {record.Dynamodb.NewImage}");
+        }
+
+        context.Logger.LogInformation("Stream processing complete.");
+    }
+}
